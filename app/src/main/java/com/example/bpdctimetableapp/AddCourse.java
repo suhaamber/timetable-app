4c1d340530f884c8d4491bef01e71489167a20b9
Suha Amber <suhaamber@gmail.com>
2021-11-16 20:54:20 +0400

    Course cards can be added and deleted

--- app/src/main/java/com/example/bpdctimetableapp/AddCourse.java
+++ app/src/main/java/com/example/bpdctimetableapp/AddCourse.java
@@ -2,6 +2,8 @@ package com.example.bpdctimetableapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,22 +13,63 @@ import java.util.ArrayList;
 public class AddCourse extends Activity {
 
     private RecyclerView recyclerViewCourse;
-    private RecyclerView.Adapter adapterCourse;
+    private NewCourseCardAdapter adapterCourse;
     private RecyclerView.LayoutManager layoutManagerCourse;
+    private ArrayList<NewCourseCard> newCourseCards;
+    private ImageButton addCourseCard;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_course);
 
-        ArrayList<NewCourseCard> newCourseCards = new ArrayList<>();
+        createCourseCards();
+        buildCourseRecyclerView();
+
+        addCourseCard = findViewById(R.id.button_add_course_card);
+
+        addCourseCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewCourseCard(v);
+            }
+        });
+
+    }
+
+    public void createCourseCards() {
+        newCourseCards = new ArrayList<>();
         //newCourseCards.add(new NewCourseCard());
 
+    }
 
+    public void buildCourseRecyclerView() {
         recyclerViewCourse = findViewById(R.id.recyclerViewClasses);
         layoutManagerCourse = new LinearLayoutManager(this);
         adapterCourse = new NewCourseCardAdapter(newCourseCards);
 
         recyclerViewCourse.setLayoutManager(layoutManagerCourse);
         recyclerViewCourse.setAdapter(adapterCourse);
+
+        adapterCourse.setOnItemClickListener(new NewCourseCardAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
+            }
+        });
+    }
+
+    public void removeItem(int position) {
+        newCourseCards.remove(position);
+        adapterCourse.notifyItemRemoved(position);
+    }
+
+    public void addNewCourseCard(View view) {
+        newCourseCards.add(new NewCourseCard());
+        adapterCourse.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/bpdctimetableapp/NewCourseCardAdapter.java
+++ app/src/main/java/com/example/bpdctimetableapp/NewCourseCardAdapter.java
@@ -3,6 +3,7 @@ package com.example.bpdctimetableapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,11 +13,49 @@ import java.util.ArrayList;
 public class NewCourseCardAdapter extends RecyclerView.Adapter<NewCourseCardAdapter.NewCourseViewHolder> {
 
     private ArrayList<NewCourseCard> newCourseCards;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
 
     public static class NewCourseViewHolder extends RecyclerView.ViewHolder{
 
-        public NewCourseViewHolder(@NonNull View itemView) {
+        public ImageButton deleteCourseCard;
+
+        public NewCourseViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
             super(itemView);
+            deleteCourseCard = itemView.findViewById(R.id.button_delete_course_card);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getBindingAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            deleteCourseCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener !=null) {
+                        int position  = getBindingAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION) {
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -24,7 +63,7 @@ public class NewCourseCardAdapter extends RecyclerView.Adapter<NewCourseCardAdap
     @Override
     public NewCourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_course_card, parent, false);
-        NewCourseViewHolder newCourseViewHolder = new NewCourseViewHolder(v);
+        NewCourseViewHolder newCourseViewHolder = new NewCourseViewHolder(v, mListener);
         return newCourseViewHolder;
     }
 
