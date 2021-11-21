d014cb421b807246d13c5b09af516756ecf6c35b
Suha Amber <suhaamber@gmail.com>
2021-11-21 11:13:45 +0400

    Models created for add course functionality

---
+++ app/src/main/java/com/example/bpdctimetableapp/EvaluationModel.java
@@ -0,0 +1,42 @@
+package com.example.bpdctimetableapp;
+
+public class EvaluationModel {
+
+    private int courseId;
+    private String evaluationType;
+    private String evaluationDate;
+
+    public EvaluationModel(int courseId, String evaluationType, String evaluationDate) {
+        this.courseId = courseId;
+        this.evaluationType = evaluationType;
+        this.evaluationDate = evaluationDate;
+    }
+
+    public EvaluationModel() {
+    //default constructor
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getEvaluationType() {
+        return evaluationType;
+    }
+
+    public void setEvaluationType(String evaluationType) {
+        this.evaluationType = evaluationType;
+    }
+
+    public String getEvaluationDate() {
+        return evaluationDate;
+    }
+
+    public void setEvaluationDate(String evaluationDate) {
+        this.evaluationDate = evaluationDate;
+    }
+}
--- app/src/main/java/com/example/bpdctimetableapp/TimetableModel.java
+++ app/src/main/java/com/example/bpdctimetableapp/TimetableModel.java
@@ -1,4 +1,52 @@
 package com.example.bpdctimetableapp;
 
 public class TimetableModel {
+
+    private int courseId;
+    private String classType;
+    private int classHour;
+    private int classDay;
+
+    public TimetableModel(int courseId, String classType, int classHour, int classDay) {
+        this.courseId = courseId;
+        this.classType = classType;
+        this.classHour = classHour;
+        this.classDay = classDay;
+    }
+
+    public TimetableModel() {
+        //default constructor
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public int getClassHour() {
+        return classHour;
+    }
+
+    public void setClassHour(int classHour) {
+        this.classHour = classHour;
+    }
+
+    public int getClassDay() {
+        return classDay;
+    }
+
+    public void setClassDay(int classDay) {
+        this.classDay = classDay;
+    }
 }
